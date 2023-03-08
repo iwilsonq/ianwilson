c9abebb436ed129c1f1bdcf15e477a1b7c567569
Ian Wilson <iwilsonq@gmail.com>
2023-03-08 02:44:17 -0800

    empty calendar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,20 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
   theme: {
     extend: {
+      minHeight: {
+        32: '128px',
+      },
       fontFamily: {
-        sans: ["Lato", ...defaultTheme.fontFamily.sans],
-        serif: ["Lora", ...defaultTheme.fontFamily.serif],
+        sans: ['Lato', ...defaultTheme.fontFamily.sans],
+        serif: ['Lora', ...defaultTheme.fontFamily.serif],
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
